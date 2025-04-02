500ae18ec2ceb434abd8471090526f64a367d64e
jstefaniak99 <149314488+jstefaniak99@users.noreply.github.com>
2025-04-02 15:19:19 +0200

    config

---
+++ read_redirect/src/main/java/com/example/project/config.java
@@ -0,0 +1,40 @@
+package com.example.project.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+@Configuration
+@EnableCassandraRepositories(basePackages = "com.example.project.repository")
+public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    @Value("${spring.data.cassandra.keyspace-name}")
+    private String keyspace;
+
+    @Value("${spring.data.cassandra.contact-points}")
+    private String contactPoints;
+
+    @Value("${spring.data.cassandra.local-datacenter}")
+    private String localDatacenter;
+
+    @Override
+    protected String getKeyspaceName() {
+        return keyspace;
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return contactPoints;
+    }
+
+    @Override
+    public String getLocalDataCenter() {
+        return localDatacenter;
+    }
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
+}
---
+++ shorturl2/src/main/java/com/example/config.java
@@ -0,0 +1,44 @@
+package com.example.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
+import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
+import org.springframework.data.cassandra.core.mapping.SimpleUserTypeResolver;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+@Configuration
+@EnableCassandraRepositories(basePackages = "com.example.repository")
+public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    @Value("${spring.data.cassandra.keyspace-name}")
+    private String keyspace;
+
+    @Value("${spring.data.cassandra.contact-points}")
+    private String contactPoints;
+
+    @Value("${spring.data.cassandra.local-datacenter}")
+    private String localDatacenter;
+
+    @Override
+    protected String getKeyspaceName() {
+        return keyspace;
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return contactPoints;
+    }
+
+    @Override
+    public String getLocalDataCenter() {
+        return localDatacenter;
+    }
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
+}
